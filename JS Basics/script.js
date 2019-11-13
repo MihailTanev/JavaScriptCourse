c43769b6fb9992d30274a96b728bcbf134a2dc33
Mihail Tanev <mihail.v.tanev@gmail.com>
2019-11-13 19:46:23 +0100

    simple calculations

--- JS Basics/script.js
+++ JS Basics/script.js
@@ -361,4 +361,153 @@ console.log(tips, finalValues);
 
 /**
  * Objects and Properties
+
+var john = {
+  firstName: "Kim",
+  lastName: "Kimions",
+  year: 1984,
+  family: ["Min", "Limn", "Jims"],
+  job: "teacher",
+  isMarried: false
+};
+
+console.log(john.firstName);
+console.log(john["firstName"]);
+
+john.job = "some text";
+console.log(john);
+
+var jane = new Object();
+jane.name = "Jane";
+jane.year = 1990;
+console.log(jane);
+*/
+
+/**
+ * Objects and Methods
+ 
+
+var john = {
+  firstName: "Kim",
+  lastName: "Kimions",
+  birthYear: 1987,
+  family: ["Min", "Limn", "Jims"],
+  job: "teacher",
+  isMarried: false,
+  calcAge: function() {
+    this.age = 2019 - this.birthYear;
+  }
+};
+
+john.calcAge();
+console.log(john);
+
+/*****************************
+ * CODING CHALLENGE 4
  */
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+*/
+/*
+var john = {
+    fullName: 'John Smith',
+    mass: 110,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+var mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+if (john.calcBMI() > mark.calcBMI()) {
+    console.log(john.fullName + ' has a higher BMI of ' + john.bmi);
+} else if (mark.bmi > john.bmi) {
+    console.log(mark.fullName + ' has a higher BMI of ' + mark.bmi);
+} else {
+    console.log('They have the same BMI');
+}
+*/
+
+/**
+ * Loops and iterations
+
+
+//for loop
+/*
+for (var i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
+
+var names = ["One", "Two", "Three", false, 1984];
+for (var i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+//while loop
+var i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
+
+
+//continue and break statements
+var names = ["One", "Two", "Three", false, 1984];
+for (var i = 0; i < names.length; i++) {
+  if (typeof names[i] !== "string") continue;
+  console.log(names[i]);
+}
+
+for (var i = 0; i < names.length; i++) {
+  if (typeof names[i] !== "string") break;
+  console.log(names[i]);
+}
+
+//looping backwards
+for (var i = names.length - 1; i >= 0; i--) {
+  console.log(names[i]);
+}
+*/
+
+/*****************************
+ * CODING CHALLENGE 5
+ */
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+*/
